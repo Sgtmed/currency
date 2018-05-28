516eb50edfe2588769a8303f9fd86a30fa62b528
Daniel Brost <danbrost@gmail.com>
2018-05-28 13:25:50 -0400

    first commit

---
+++ currency.js
@@ -0,0 +1,6 @@
+/* eslint "no-console": 0 */
+const accounting = require('accounting');
+/* create a variable and assign it a numeric value */
+const amount = 98.3456;
+
+console.log(accounting.formatMoney(amount));
---
+++ index.js
@@ -0,0 +1,4 @@
+var moment = require('moment');
+
+console.log('Hello From JS');
+console.log(moment().startOf('day').fromNow());
